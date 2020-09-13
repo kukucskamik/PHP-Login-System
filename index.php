3060787500c4c1cd41e0db78f60ebbb7b46d980e
MIKI <kukucskamiklos@gmail.com>
2020-09-13 15:25:27 +0200

    Some changes

---
+++ index.php
@@ -0,0 +1,52 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Title</title>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/css/uikit.min.css" />
+
+
+</head>
+<body>
+
+    <div class="uk-section uk-container">
+        <div class="uk-grid uk-child-width-l-3@s uk-child-width-l-l" uk-grid>
+        <form class="uk-form-stacked js-login">
+
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">Email</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com" required="required">
+        </div>
+    </div>
+    <div class="uk-margin">
+        <label class="uk-form-label" for="form-stacked-text">Password</label>
+        <div class="uk-form-controls">
+            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your Password" required="required">
+        </div>
+    </div>
+
+    <div class="uk-margin">
+        <button class="uk-button uk-button-default" type="submit">Login</button>
+        </div>
+    </div>
+
+    
+
+</form>
+        </div>
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.7/dist/js/uikit-icons.min.js"></script>
+
+</body>
+</html>
